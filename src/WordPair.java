a66f1ecb97662c03d5e4e1f7b1a96dce88997ffd
Addison Lee <leea9873@gmail.com>
2023-09-15 09:24:32 +0800

    Initial commit

---
+++ src/WordPair.java
@@ -0,0 +1,24 @@
+public class WordPair {
+    private String first;
+    private String second;
+    public WordPair(String first, String second)
+    {
+
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getFirst()
+    {
+        return first;
+        // To be implemented.
+
+    }
+
+    public String getSecond()
+    {
+        return second;
+        // To be implemented.
+
+    }
+}
